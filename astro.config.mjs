923c7d2f29749f8364f991d1501a3722b28dadf9
Brendan Callanan <30944704+bennycallanan@users.noreply.github.com>
2026-09-17 17:49:54 +0100

    eol: removing final bloat from website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 // @ts-check
-import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
 	site: 'https://www.bcqstudios.com',
-	integrations: [sitemap()],
 	vite: {
 		plugins: [tailwindcss()],
 	},
